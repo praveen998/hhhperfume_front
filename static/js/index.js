5e751e736b3109d27541f0ec8185f67fd5326320
praveen <praveen.gopi717@gmail.com>
2025-03-04 14:12:08 +0530

    newmarch4

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 
 function seturl() {
-    localStorage.setItem("fasturl", "https://6886-2402-3a80-e15-5676-d9eb-2f53-2d6c-c2e9.ngrok-free.app");
+    localStorage.setItem("fasturl", "https://7036-45-115-89-138.ngrok-free.app");
 }
 
 function setweburl() {
